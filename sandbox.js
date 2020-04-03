2141bf91be11af6057340a5786689728f05a6093
Alex.Kozyura <alex-kzr@github.com>
2020-04-03 11:21:59 +1000

    Add object spread syntax

--- sandbox.js
+++ sandbox.js
@@ -1,3 +1,5 @@
-const people = ['mario', 'luigi', 'yoshi'];
-const members = ['ryu', 'chun-li', ...people];
-console.log(members); // ['ryu', 'chun-li', 'mario', 'luigi', 'yoshi'];
+const person = { name: 'alex', age: 30, job: 'net ninja'}
+const personPointer = person;
+const personClone1 = {...person} // brand new object, not pointer
+const personClone2 = {...person, location: 'London'}
+console.log(personPointer == person, personClone1 == person);
